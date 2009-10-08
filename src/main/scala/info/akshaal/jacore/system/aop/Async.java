0516911d674bff174f5ef5a2284d755f995eae64
(no author) <(no author)@508b99f4-6b71-4659-bec4-6d750112b50a>
2009-10-08 18:38:28 +0000

    ...
    git-svn-id: svn://fw.home.toril.ru/jacore/trunk@1097 508b99f4-6b71-4659-bec4-6d750112b50a

---
+++ src/main/scala/info/akshaal/jacore/system/aop/Async.java
@@ -0,0 +1,17 @@
+/*
+ * Async.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package info.akshaal.jacore.system.aop;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+
+@Retention (RetentionPolicy.RUNTIME)
+@Target (ElementType.METHOD)
+@interface Async {}
