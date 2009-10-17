c5693b1d39febc1a277524d6a0ee9292734341b7
(no author) <(no author)@508b99f4-6b71-4659-bec4-6d750112b50a>
2009-10-17 15:17:29 +0000

    ...
    git-svn-id: svn://fw.home.toril.ru/jacore/trunk@1111 508b99f4-6b71-4659-bec4-6d750112b50a

--- src/main/scala/info/akshaal/jacore/system/actor/Actor.scala
+++ src/main/scala/info/akshaal/jacore/system/actor/Actor.scala
@@ -5,19 +5,15 @@ package actor
 import org.jetlang.fibers.PoolFiberFactory
 import org.jetlang.core.BatchExecutor
 
-import java.lang.reflect.Modifier
-
 import Predefs._
 import logger.Logging
-import annotation.{Act, ExtractBy}
 
 /**
  * Implementation of actors.
  *
  * @param actorEnv environment for actor
  */
-abstract class Actor (actorEnv : ActorEnv)
-                extends Logging with NotNull
+abstract class Actor (actorEnv : ActorEnv) extends Logging with NotNull
 {
     /**
      * Method dispatcher. Forwards message processing request to
@@ -28,15 +24,13 @@ abstract class Actor (actorEnv : ActorEnv)
     /**
      * Schedule to be used by this actor.
      */
-    protected final val schedule =
-                    new ActorSchedule (this, actorEnv.scheduler)
+    protected final val schedule = new ActorSchedule (this, actorEnv.scheduler)
 
     /**
      * A fiber used by this actor.
      */
     private[this] final val fiber =
-        new PoolFiberFactory (actorEnv.pool.executors)
-                        .create (new ActorExecutor (this))
+        new PoolFiberFactory (actorEnv.pool.executors).create (new ActorExecutor (this))
 
     /**
      * Current sender. Only valid when act method is called.
@@ -48,8 +42,7 @@ abstract class Actor (actorEnv : ActorEnv)
      * messages handled by actor. Function returns by default actor
      * processes no messages.
      */
-    protected def act(): PartialFunction[Any, Unit] =
-                    Actor.defaultActMessageHandler
+    protected def act(): PartialFunction[Any, Unit] = Actor.defaultActMessageHandler
 
     /**
      * Queue message for processing by this actor. This is alias for ! method.
@@ -96,6 +89,7 @@ abstract class Actor (actorEnv : ActorEnv)
             msg match {
                 case Ping => sentFrom.foreach (_ ! Pong)
                 case call @ Call (inv) => CallByMessageMethodInterceptor.call (call)
+                case other if dispatcher.dispatch(msg) => ()
                 case other if act.isDefinedAt (msg) => act () (msg)
                 case other => warn ("Ignored message: " + msg)
             }
@@ -126,140 +120,13 @@ abstract class Actor (actorEnv : ActorEnv)
      * annotated method of this object. It also subscribes.
      */
     private def createDispatcherAndSubscribe : MethodDispatcher = {
-        // Check methods and collect information
-        var methods : List[ActMethodDesc] = Nil
+        val actMethods = ActorClassScanner.scan (this)
 
-        // For each annotated method (including inherited) of this class
-        val allMethodNames = getClass.getMethods.map (_.getName)
-
-        for (method <- getClass.getMethods if method.isAnnotationPresent (classOf[Act])) {
-            val methodName = method.getName
-
-            def unrecover (str : String) {
-                throw new UnrecoverableError ("Action method " + methodName + " " + str)
-            }
-
-            // Checks modifiers
-            val modifiers = method.getModifiers
-
-            if (Modifier.isStatic (modifiers)) {
-                unrecover ("must not be static")
-            }
-
-            // Check return type
-            val returnType = method.getReturnType
-            if (returnType != Void.TYPE) {
-                unrecover ("must return nothing, but returns " + returnType.getName)
-            }
-
-            // Check uniqueness of method name
-            if (allMethodNames.count(_ == methodName) > 1) {
-                unrecover ("must not be overloaded")
-            }
-
-            // Check params
-            val paramTypes = method.getParameterTypes
-            if (paramTypes.length == 0) {
-                unrecover ("must have at least one argument")
-            }
-
-            val paramExtractors =
-                method.getParameterAnnotations.map (
-                        _.find (_.isInstanceOf [ExtractBy])
-                         .map (_.asInstanceOf [ExtractBy].value())
-                    )
-
-            val paramDescs =
-                for ((paramClazz, paramExtractor) <- paramTypes.zip (paramExtractors))
-                    yield ActMethodParamDesc (clazz = paramClazz,
-                                              extractor = paramExtractor)
-
-            if (!paramDescs.head.extractor.isEmpty) {
-                unrecover ("can't have extraction as a first argument."
-                           + " First argument must be a message method receives.")
-            }
-
-            if (paramDescs.count (_.extractor.isEmpty) > 1) {
-                unrecover ("must have no more than one argument without extractor")
-            }
-
-            // Create matcher
-            val acceptMessageClass = paramDescs.head.clazz
-            val messageExtractions =
-                    for (paramDesc <- paramDescs.tail)
-                            yield MessageExtraction (acceptExtractionClass = paramDesc.clazz,
-                                                     messageExtractor = paramDesc.extractor.get)
-
-            for (messageExtraction <- messageExtractions) {
-                val extractor = messageExtraction.messageExtractor
-                val acceptExtractionClass = messageExtraction.acceptExtractionClass
-
-                if (acceptExtractionClass.isPrimitive) {
-                    unrecover ("can't use primitive types for extractions")
-                }
-
-                if (!classOf[MessageExtractor[Any, Any]].isAssignableFrom(extractor)) {
-                    unrecover ("has an extractor not implementing MessageExtractor interface: "
-                               + extractor)
-                }
-
-                val extractingMethods =
-                        extractor.getMethods
-                                 .filter (m => m.getName == "extractFrom" && !m.isSynthetic)
-                if (extractingMethods.length > 1) {
-                    unrecover ("uses an extractor with overloaded extractFrom method: "
-                               + extractor)
-                }
-
-                val extractorMethod = extractingMethods.head
-                val extractorMethodArg = extractorMethod.getParameterTypes()(0)
-                val extractorMethodReturn = extractorMethod.getReturnType
-
-                if (!extractorMethodArg.isAssignableFrom(acceptMessageClass)) {
-                    unrecover ("uses extractor " + extractor
-                               + " which can't handle messages of class " + acceptMessageClass)
-                }
-
-                if (!extractorMethodReturn.isAssignableFrom(acceptExtractionClass)
-                    && !acceptExtractionClass.isAssignableFrom(extractorMethodReturn))
-                {
-                    unrecover ("uses extractor " + extractor
-                               + " which produces values of class incompatible"
-                               + " to the class of extraction (argument)")
-                }
-            }
-
-            val messageMatcher =
-                    MessageMatcher (acceptMessageClass = acceptMessageClass,
-                                    messageExtractions = Set(messageExtractions : _*))
-
-            if (messageMatcher.messageExtractions.size != paramDescs.length - 1) {
-                unrecover ("must not have duplicated arguments")
-            }
-
-            // Gather information
-            val actAnnotation = method.getAnnotation (classOf[Act])
-
-            methods ::= ActMethodDesc (name = methodName,
-                                       subscribe = actAnnotation.subscribe,
-                                       params = paramDescs,
-                                       matcher = messageMatcher)
-
-        }
-
-        debugLazy ("Found action methods " + methods)
-
-        // Sanity checks on class level
-        for ((_, methodGroup) <- methods.groupBy (_.matcher)) {
-            if (methodGroup.length > 1) {
-                throw new UnrecoverableError ("More than one mathod match the same messages: "
-                                              + methodGroup.map(_.name).mkString(" "))
+        new MethodDispatcher () {
+            override def dispatch (msg : Any) : Boolean = {
+                false
             }
         }
-
-        // TODO: sortWith to order
-
-        null
     }
 
     /**
@@ -267,21 +134,16 @@ abstract class Actor (actorEnv : ActorEnv)
      * a message processing request to an annotated method of the
      * enclosed class.
      */
-    protected abstract class MethodDispatcher {
-        def dispatch (msg : Any) : Unit
+    protected abstract class MethodDispatcher extends NotNull {
+        /**
+         * Dispatch message to one of methods annotated with @Act annotation.
+         *
+         * @param msg message to process
+         * @return true if message has been forwarded
+         */
+        def dispatch (msg : Any) : Boolean
     }
 }
-
-/**
- * Describes a method annotated with @Act annotation.
- */
-private[actor] sealed case class ActMethodDesc (name : String,
-                                                subscribe : Boolean,
-                                                params : Seq[ActMethodParamDesc],
-                                                matcher : MessageMatcher)
-
-private[actor] sealed case class ActMethodParamDesc (clazz : Class[_ <: Any],
-                                                     extractor : Option[Class[_ <: Any]])
 
 /**
  * Executor of queued actors.
---
+++ src/main/scala/info/akshaal/jacore/system/actor/ActorClassScanner.scala
@@ -0,0 +1,180 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package info.akshaal.jacore
+package system
+package actor
+
+import Predefs._
+import logger.Logging
+
+import java.lang.reflect.Modifier
+import annotation.{Act, ExtractBy}
+
+/**
+ * The sole role of this class is to scan Actor classes for methods annotated with @Act
+ * annotation and do some sanity checks.
+ */
+private[actor] object ActorClassScanner extends Logging {
+    /**
+     * Scan actor for messages annotated with @Act annotation and validate these methods.
+     * @param actor actor to scan
+     * @return method descriptions for annotated methods
+     */
+    def scan (actor : Actor) : Seq[ActMethodDesc] = {
+        val actorClass = actor.getClass
+
+        def badClass (str : String) {
+            throw new UnrecoverableError ("Actor class " + actorClass.getName + ": " + str)
+        }
+
+        // Check methods and collect information
+        var methods : List[ActMethodDesc] = Nil
+
+        // For each annotated method (including inherited) of this class
+        val allMethodNames = actorClass.getMethods.map (_.getName)
+
+        for (method <- actorClass.getMethods if method.isAnnotationPresent (classOf[Act])) {
+            val methodName = method.getName
+
+            def badMethod (str : String) {
+                badClass ("Action method " + methodName + " " + str)
+            }
+
+            // Checks modifiers
+            val modifiers = method.getModifiers
+
+            if (Modifier.isStatic (modifiers)) {
+                badMethod ("must not be static")
+            }
+
+            // Check return type
+            val returnType = method.getReturnType
+            if (returnType != Void.TYPE) {
+                badMethod ("must return nothing, but returns " + returnType.getName)
+            }
+
+            // Check uniqueness of method name
+            if (allMethodNames.count(_ == methodName) > 1) {
+                badMethod ("must not be overloaded")
+            }
+
+            // Check params
+            val paramTypes = method.getParameterTypes
+            if (paramTypes.length == 0) {
+                badMethod ("must have at least one argument")
+            }
+
+            val paramExtractors =
+                method.getParameterAnnotations.map (
+                        _.find (_.isInstanceOf [ExtractBy])
+                         .map (_.asInstanceOf [ExtractBy].value())
+                    )
+
+            val paramDescs =
+                for ((paramClazz, paramExtractor) <- paramTypes.zip (paramExtractors))
+                    yield ActMethodParamDesc (clazz = paramClazz,
+                                              extractor = paramExtractor)
+
+            if (!paramDescs.head.extractor.isEmpty) {
+                badMethod ("can't have extraction as a first argument."
+                           + " First argument must be a message method receives.")
+            }
+
+            if (paramDescs.count (_.extractor.isEmpty) > 1) {
+                badMethod ("must have no more than one argument without extractor")
+            }
+
+            // Create matcher
+            val acceptMessageClass = paramDescs.head.clazz
+            val messageExtractions =
+                    for (paramDesc <- paramDescs.tail)
+                            yield MessageExtraction (acceptExtractionClass = paramDesc.clazz,
+                                                     messageExtractor = paramDesc.extractor.get)
+
+            for (messageExtraction <- messageExtractions) {
+                val extractor = messageExtraction.messageExtractor
+                val acceptExtractionClass = messageExtraction.acceptExtractionClass
+
+                if (acceptExtractionClass.isPrimitive) {
+                    badMethod ("can't use primitive types for extractions")
+                }
+
+                if (!classOf[MessageExtractor[Any, Any]].isAssignableFrom(extractor)) {
+                    badMethod ("has an extractor not implementing MessageExtractor interface: "
+                               + extractor)
+                }
+
+                val extractingMethods =
+                        extractor.getMethods
+                                 .filter (m => m.getName == "extractFrom" && !m.isSynthetic)
+                if (extractingMethods.length > 1) {
+                    badMethod ("uses an extractor with overloaded extractFrom method: "
+                               + extractor)
+                }
+
+                val extractorMethod = extractingMethods.head
+                val extractorMethodArg = extractorMethod.getParameterTypes()(0)
+                val extractorMethodReturn = extractorMethod.getReturnType
+
+                if (!extractorMethodArg.isAssignableFrom(acceptMessageClass)) {
+                    badMethod ("uses extractor " + extractor
+                               + " which can't handle messages of class " + acceptMessageClass)
+                }
+
+                if (!extractorMethodReturn.isAssignableFrom(acceptExtractionClass)
+                    && !acceptExtractionClass.isAssignableFrom(extractorMethodReturn))
+                {
+                    badMethod ("uses extractor " + extractor
+                               + " which produces values of class incompatible"
+                               + " to the class of extraction (argument)")
+                }
+            }
+
+            val messageMatcher =
+                    MessageMatcher (acceptMessageClass = acceptMessageClass,
+                                    messageExtractions = Set(messageExtractions : _*))
+
+            if (messageMatcher.messageExtractions.size != paramDescs.length - 1) {
+                badMethod ("must not have duplicated arguments")
+            }
+
+            // Gather information
+            val actAnnotation = method.getAnnotation (classOf[Act])
+
+            methods ::= ActMethodDesc (name = methodName,
+                                       subscribe = actAnnotation.subscribe,
+                                       params = paramDescs,
+                                       matcher = messageMatcher)
+
+        }
+
+        debugLazy ("Found action methods " + methods)
+
+        // Sanity checks on class level
+        for ((_, methodGroup) <- methods.groupBy (_.matcher)) {
+            if (methodGroup.length > 1) {
+                badClass ("More than one mathod match the same messages: "
+                          + methodGroup.map(_.name).mkString(" "))
+            }
+        }
+
+        methods;
+    }
+}
+
+/**
+ * Describes a method annotated with @Act annotation.
+ */
+private[actor] sealed case class ActMethodDesc (name : String,
+                                                subscribe : Boolean,
+                                                params : Seq[ActMethodParamDesc],
+                                                matcher : MessageMatcher)
+
+/**
+ * Describes an argument of method annotated with @Act annotation.
+ */
+private[actor] sealed case class ActMethodParamDesc (clazz : Class[_ <: Any],
+                                                     extractor : Option[Class[_ <: Any]])
