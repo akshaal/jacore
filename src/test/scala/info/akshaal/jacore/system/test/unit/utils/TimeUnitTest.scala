0c94bf3a37b5094a636653126b4c791184a41469
(no author) <(no author)@508b99f4-6b71-4659-bec4-6d750112b50a>
2009-11-13 18:34:41 +0000

    specs support
    git-svn-id: svn://fw.home.toril.ru/jacore/trunk@1158 508b99f4-6b71-4659-bec4-6d750112b50a

--- src/test/scala/info/akshaal/jacore/system/test/unit/utils/TimeUnitTest.scala
+++ src/test/scala/info/akshaal/jacore/system/test/unit/utils/TimeUnitTest.scala
@@ -7,85 +7,80 @@ package system.test.unit.utils
 
 import Predefs._
 
-/*
-class TimeUnitTest extends BaseUnitTest {
-    @Test (groups = Array("unit"))
-    def testConstruction () = {
-        assertEquals (10.nanoseconds.asNanoseconds, 10L)
-        assertEquals (11.microseconds.asNanoseconds, 11L * 1000L)
-        assertEquals (12.milliseconds.asNanoseconds, 12L * 1000L * 1000L)
-        assertEquals (13.seconds.asNanoseconds, 13L * 1000L * 1000L * 1000L)
-        assertEquals (14.minutes.asNanoseconds, 14L * 1000L * 1000L * 1000L * 60L)
-        assertEquals (15.hours.asNanoseconds, 15L * 1000L * 1000L * 1000L * 60L * 60L)
+import org.specs.SpecificationWithJUnit
+
+class TimeUnitTest extends SpecificationWithJUnit ("TimeUnit class specification") {
+    "TimeUnit" should {
+        "allow conversions to nanoseconds" in {
+            10.nanoseconds.asNanoseconds   must_==  10L
+            11.microseconds.asNanoseconds  must_==  11L * 1000L
+            12.milliseconds.asNanoseconds  must_==  12L * 1000L * 1000L
+            13.seconds.asNanoseconds       must_==  13L * 1000L * 1000L * 1000L
+            14.minutes.asNanoseconds       must_==  14L * 1000L * 1000L * 1000L * 60L
+            15.hours.asNanoseconds         must_==  15L * 1000L * 1000L * 1000L * 60L * 60L
+        }
+
+        "support equality operation" in {
+            1.milliseconds  must_==  1.milliseconds
+            1.milliseconds  must_!=  10.milliseconds
+            2.hours         must_==  2.hours
+            2.hours         must_!=  1.hours
+            3.minutes       must_==  3.minutes
+            3.minutes       must_!=  1.minutes
+            30.seconds      must_==  30.seconds
+            30.seconds      must_==  30000.milliseconds
+            30.seconds      must_!=  1.seconds
+            30.seconds      must_!=  30.minutes
+        }
+
+        "support arithmetics" in {
+            23.hours + 1.hours                 must_==  24.hours
+            50.minutes + 10.minutes + 3.hours  must_==  4.hours
+            60.seconds + 1.minutes             must_==  120.seconds
+        }
+
+        "have meaningful toString method" in {
+            "2days 23hours 1mins 45secs 15ms 10us 100ns"  must_==
+                (48.hours + 23.hours + 1.minutes + 45.seconds
+                 + 15.milliseconds + 10.microseconds + 100.nanoseconds).toString
+
+            "2hours 5secs 7us"  must_==
+                (2.hours + 5.seconds + 7.microseconds).toString
+                          
+            "11mins 33ms 55ns"  must_==
+                (11.minutes + 33.milliseconds + 55.nanoseconds).toString
+            
+            "1mins"  must_==  60.seconds.toString
+
+            "0ns"  must_==  0.seconds.toString
+        }
+
+        "support construction from string" in {
+            "123 nanoseconds".asNanoseconds  must_==  123.nanoseconds.asNanoseconds
+            "99 microseconds".asNanoseconds  must_==  99.microseconds.asNanoseconds
+            "1 milliseconds".asNanoseconds   must_==  1.milliseconds.asNanoseconds
+            "30 seconds".asNanoseconds       must_==  30.seconds.asNanoseconds
+            "10 minutes".asNanoseconds       must_==  10.minutes.asNanoseconds
+            "5 hours".asNanoseconds          must_==  5.hours.asNanoseconds
+
+            "123 nanoseconds 1 hours".asNanoseconds  must_==
+                          (123.nanoseconds + 1.hours).asNanoseconds
+
+            "99 microseconds 45 seconds".asNanoseconds  must_==
+                          (99.microseconds + 45.seconds).asNanoseconds
+
+            "2 minutes 1 milliseconds 4 seconds".asNanoseconds  must_==
+                          (2.minutes + 1.milliseconds + 4.seconds).asNanoseconds
+
+            "30 seconds 1 milliseconds".asNanoseconds  must_==
+                          (30.seconds + 1.milliseconds).asNanoseconds
+
+            "10 minutes 11 hours".asNanoseconds  must_==
+                          (10.minutes + 11.hours).asNanoseconds
+        }
+
+        "not parse broken strings" in {
+            "30 secons 1 milliseconds".asNanoseconds must throwA[IllegalArgumentException]
+        }
     }
-
-    @Test (groups = Array("unit"))
-    def testEquals () = {
-        assertEquals (1.milliseconds, 1.milliseconds)
-        assertTrue (1.milliseconds != 10.milliseconds)
-        assertEquals (2.hours, 2.hours)
-        assertTrue (2.hours != 1.hours)
-        assertEquals (3.minutes, 3.minutes)
-        assertTrue (3.minutes != 1.minutes)
-        assertEquals (30.seconds, 30.seconds)
-        assertEquals (30.seconds, 30000.milliseconds)
-        assertTrue (30.seconds != 1.seconds)
-        assertTrue (30.seconds != 30.minutes)
-    }
-
-    @Test (groups = Array("unit"))
-    def testArith () = {
-        assertEquals (23.hours + 1.hours, 24.hours)
-        assertEquals (50.minutes + 10.minutes + 3.hours, 4.hours)
-        assertEquals (60.seconds + 1.minutes, 120.seconds)
-    }
-
-    @Test (groups = Array("unit"))
-    def testToString () = {
-        assertEquals ((48.hours + 23.hours + 1.minutes + 45.seconds
-                      + 15.milliseconds + 10.microseconds + 100.nanoseconds).toString,
-                      "2days 23hours 1mins 45secs 15ms 10us 100ns")
-
-        assertEquals ((2.hours + 5.seconds + 7.microseconds).toString,
-                      "2hours 5secs 7us")
-
-        assertEquals ((11.minutes + 33.milliseconds + 55.nanoseconds).toString,
-                      "11mins 33ms 55ns")
-
-        assertEquals (60.seconds.toString, "1mins")
-        
-        assertEquals (0.seconds.toString, "0ns")
-    }
-
-    @Test (groups = Array("unit"))
-    def testStringParser () = {
-        assertEquals ("123 nanoseconds".asNanoseconds, 123.nanoseconds.asNanoseconds)
-        assertEquals ("99 microseconds".asNanoseconds, 99.microseconds.asNanoseconds)
-        assertEquals ("1 milliseconds".asNanoseconds, 1.milliseconds.asNanoseconds)
-        assertEquals ("30 seconds".asNanoseconds, 30.seconds.asNanoseconds)
-        assertEquals ("10 minutes".asNanoseconds, 10.minutes.asNanoseconds)
-        assertEquals ("5 hours".asNanoseconds, 5.hours.asNanoseconds)
-
-        assertEquals ("123 nanoseconds 1 hours".asNanoseconds,
-                      (123.nanoseconds + 1.hours).asNanoseconds)
-
-        assertEquals ("99 microseconds 45 seconds".asNanoseconds,
-                      (99.microseconds + 45.seconds).asNanoseconds)
-
-        assertEquals ("2 minutes 1 milliseconds 4 seconds".asNanoseconds,
-                      (2.minutes + 1.milliseconds + 4.seconds).asNanoseconds)
-
-        assertEquals ("30 seconds 1 milliseconds".asNanoseconds,
-                      (30.seconds + 1.milliseconds).asNanoseconds)
-
-        assertEquals ("10 minutes 11 hours".asNanoseconds,
-                      (10.minutes + 11.hours).asNanoseconds)
-    }
-
-    @Test (expectedExceptions = Array(classOf[IllegalArgumentException]))
-    def failedParsing () = {
-        "30 secons 1 milliseconds".asNanoseconds
-
-        assertFalse (true, "Exception should have been thrown")
-    }
-}*/
+}
