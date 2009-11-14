3ece8a296dbe0460ee0d1f2bf40d807acda350a7
(no author) <(no author)@508b99f4-6b71-4659-bec4-6d750112b50a>
2009-11-14 13:12:38 +0000

    a...
    git-svn-id: svn://fw.home.toril.ru/jacore/trunk@1159 508b99f4-6b71-4659-bec4-6d750112b50a

---
+++ src/test/scala/info/akshaal/jacore/system/test/unit/logger/LoggerTest.scala
@@ -0,0 +1,51 @@
+/** Akshaal (C) 2009. GNU GPL. http://akshaal.info */
+
+package info.akshaal.jacore
+package system
+package test.unit.logger
+
+import org.specs.SpecificationWithJUnit
+
+import logger.Logging
+
+class LoggerTest extends SpecificationWithJUnit ("Logging trait specification")
+{
+    "Logging" should {
+        "provide Logger instance" in {
+            object T extends Logging {
+                logger.debug ("Debug level")
+                logger.info ("Info level")
+                logger.warn ("Warn level")
+                logger.error ("Error level")
+
+                logger.debugLazy (sideEffect ("debug lazy"))
+                logger.infoLazy (sideEffect ("info lazy"))
+                logger.warnLazy (sideEffect ("warn lazy"))
+                logger.errorLazy (sideEffect ("error lazy"))
+            }
+            
+            T must not be null
+        }
+
+        "provide Logger methods as mixins" in {
+            object T extends Logging {
+                debug ("2 Debug level")
+                info ("2 Info level")
+                warn ("2 Warn level")
+                error ("2 Error level")
+
+                debugLazy (sideEffect ("2 debug lazy"))
+                infoLazy (sideEffect ("2 info lazy"))
+                warnLazy (sideEffect ("2 warn lazy"))
+                errorLazy (sideEffect ("2 error lazy"))
+            }
+            
+            T must not be null
+        }
+    }
+ 
+    private def sideEffect (str : String) = {
+        logger.debug ("Computing argument for lazy log message: " + str)
+        str
+    }
+}
