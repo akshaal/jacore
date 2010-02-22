2510d81f9c3064ba6d7f7f37ea9498c2be908f03
Evgeny Chukreev <akshaal@akshaal.info>
2010-02-22 21:45:27 +0200

    DoubleValueFrame added

---
+++ src/main/scala/info/akshaal/jacore/utils/DoubleValueFrame.scala
@@ -0,0 +1,60 @@
+/*
+ * ValueFrame.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package info.akshaal.jacore
+package utils
+
+/**
+ * Frame of values with maximum fixed number of elements.
+ * Frame maintains sum of elements.
+ */
+final class DoubleValueFrame (maximum : Int) extends NotNull {
+    require (maximum > 0, "maximum must be positive number")
+
+    private val array = new Array[Double] (maximum)
+    private var pos = -1
+    private var count = 0
+    private var sum = 0d
+
+    /**
+     * Add value.
+     * @param value value
+     */
+    def put (value : Double) = {
+        pos += 1
+        
+        if (pos == maximum) {
+            pos = 0;
+        }
+
+        if (count < maximum) {
+            count += 1
+            sum += value
+        } else {
+            sum += value - array(pos)
+        }
+
+        array (pos) = value
+    }
+
+   /**
+     * @return the average
+     */
+    def average () : Double = {
+        if (count == 0) 0d else sum / count
+    }
+
+    /**
+     * Returns current count.
+     */
+    def currentCount : Int = count
+
+    /**
+     * Returns current sum.
+     */
+    def currentSum : Double = sum
+}
---
+++ src/test/scala/info/akshaal/jacore/test/unit/utils/DoubleValuesFrameTest.scala
@@ -0,0 +1,77 @@
+/**
+ * Akshaal (C) 2009. GNU GPL. http://akshaal.info
+ */
+
+package info.akshaal.jacore
+package test.unit.utils
+
+import org.specs.SpecificationWithJUnit
+
+import utils.DoubleValueFrame
+
+/**
+ * Test Double Value Frame.
+ */
+class DoubleValuesFrameTest extends SpecificationWithJUnit ("DoubleValueFrame class specification")
+{
+    "DoubleValueFrame" should {
+        "fail with zero width" in {
+            new DoubleValueFrame (0) must throwA[IllegalArgumentException]
+        }
+
+        "fail with negative width" in {
+            new DoubleValueFrame (-1) must throwA[IllegalArgumentException]
+        }
+
+        "support frames with size 1" in {
+            val frame = new DoubleValueFrame (1)
+            frame.average must_== 0.0d
+
+            frame.put (1.0d)
+            frame.average must_== 1.0d
+
+            frame.put (1.0d)
+            frame.average must_== 1.0d
+
+            frame.put (2.0d)
+            frame.average must_== 2.0d
+
+            frame.put (3.0d)
+            frame.average must_== 3.0d
+        }
+
+        "support frames with size 2" in {
+            val frame = new DoubleValueFrame (2)
+            frame.average must_== 0.0d
+
+            frame.put (1.0d)
+            frame.average must_== 1.0d
+
+            frame.put (1.0d)
+            frame.average must_== 1.0d
+
+            frame.put (2.0d)
+            frame.average must_== 3.0d / 2.0d
+
+            frame.put (3.0d)
+            frame.average must_== 5.0d / 2.0d
+        }
+
+        "support frames with size 3" in {
+            val frame = new DoubleValueFrame (3)
+            frame.average must_== 0.0d
+
+            frame.put (1.0d)
+            frame.average must_== 1.0d
+
+            frame.put (1.0d)
+            frame.average must_== 1.0d
+
+            frame.put (2.0d)
+            frame.average must_== 4.0d / 3.0d
+
+            frame.put (3.0d)
+            frame.average must_== 6.0d / 3.0d
+        }
+    }
+}
