126649309ae13d7ebebbcd614d90e25b34b793cf
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2011-01-16 23:13:24 +0200

    Get rid of ibatis/mybatis support.

--- src/main/scala/info/akshaal/jacore/dao/common.scala
+++ src/main/scala/info/akshaal/jacore/dao/common.scala
@@ -1,7 +1,9 @@
-/** Akshaal (C) 2009-2010. GNU GPL. http://akshaal.info */
+/** Akshaal (C) 2009-2011. GNU GPL. http://akshaal.info */
 
 package info.akshaal.jacore
 package dao
+
+import actor.Operation
 
 /**
  * Interface for data inserter.
@@ -11,18 +13,5 @@ trait DataInserter[T] {
      * Insert data asynchronously.
      * @param data data to insert
      */
-    def insert (data : T) : Unit
-
-    /**
-     * Insert data asynchronously. When insertion is finished, a notification message
-     * InsertFinished will be issued with the payload.
-     * @param data data to insert
-     * @param payload to send back in notification message
-     */
-    def insert (data : T, payload : Any) : Unit
+    def insertOperation (data : T) : Operation.WithResult [Unit]
 }
-
-/**
- * Issued to the requester when insert is finished.
- */
-sealed case class InsertFinished (payload : Any)
