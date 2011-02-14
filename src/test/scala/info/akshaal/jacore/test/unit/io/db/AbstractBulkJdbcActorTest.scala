da97f44199599346fb60e2c591ae8886b02684ad
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2011-02-14 22:22:38 +0200

    (Empty) test for AbstractBulkJdbcActor

---
+++ src/test/scala/info/akshaal/jacore/test/unit/io/db/AbstractBulkJdbcActorTest.scala
@@ -0,0 +1,20 @@
+/** Akshaal (C) 2009-2011. GNU GPL. http://akshaal.info */
+
+package info.akshaal.jacore
+package test
+package unit.io.db
+
+import unit.UnitTestHelper._
+import io.db.AbstractBulkJdbcActor
+
+class AbstractBulkJdbcActorTest extends JacoreSpecWithJUnit ("AbstractBulkJdbcActor specification") {
+    import AbstractBulkJdbcActorTest._
+
+    "AbstractBulkJdbcActor" should {
+        "make it possible to run any SQL supported by DB" in {
+        }
+    }
+}
+
+object AbstractBulkJdbcActorTest {
+}
