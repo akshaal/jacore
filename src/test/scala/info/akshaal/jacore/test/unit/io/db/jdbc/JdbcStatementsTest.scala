cd8d76d97843ae69470733f0399d4f759e35a0ae
Evgeny Chukreev <Evgeny.Chukreev@gmail.com>
2011-08-01 17:49:12 +0300

    More tests.

--- src/test/scala/info/akshaal/jacore/test/unit/io/db/jdbc/JdbcStatementsTest.scala
+++ src/test/scala/info/akshaal/jacore/test/unit/io/db/jdbc/JdbcStatementsTest.scala
@@ -12,6 +12,7 @@ import Statement.{ProvidedValue => Prov}
 
 class JdbcStatementsTest extends JacoreSpecWithJUnit ("Statement specification") {
     case class X (x : Int, y : String)
+    case class Y (a : String, b : Int)
 
     val s0_1 : Statement0 [Domainless] = "select 1"
     val s0_2 : Statement0 [Domainless] = "set xxx"
@@ -42,8 +43,11 @@ class JdbcStatementsTest extends JacoreSpecWithJUnit ("Statement specification")
     val s10 = s9 ++ (JdbcLong, 10L) ++ JdbcInt ++ "x" ++ (JdbcFloat, 1.0f)
 
     val sd0_1 = classOf [X] /: s0_1 +++ (JdbcInt, ((_ : X).x))
-    val sd0_2 = classOf [X] /: s0_1 +++ (JdbcInt, _.x) +++ (JdbcString, _.y)
+    val sd0_2 = classOf [Y] /: s0_2 +++ (JdbcString, _.a) +++ (JdbcInt, _.b)
     val sd0_3 = classOf [X] /: classOf [X] /: s0_1 +++ (JdbcInt, _.x) ++ "abc" +++ (JdbcString, _.y)
+
+    val sd1 = sd0_2 ++ JdbcRef
+    val sd10 = classOf[X] /: s10 +++ (JdbcString, _.y)
 
     // Check statement
     def checkStmt (stmt : Statement [_], sql : String, pvs : Any*) : Unit = {
@@ -58,6 +62,19 @@ class JdbcStatementsTest extends JacoreSpecWithJUnit ("Statement specification")
         placeholder  must_==  Hdr (jdbcType, pos)
     }
 
+    // Get manifest of variable
+    def manifestOf [M] (v : M) (implicit m : Manifest [M]) : Manifest [M] = m
+
+    // Check type arguments
+    def checkArgs (manifest : Manifest [_], args : Manifest [_]*) : Unit = {
+        manifest.typeArguments  must_==  args.toList
+    }
+
+    // Check type arguments of value
+    def checkArgs [V] (v : V, args : Manifest [_]*) (implicit manifest : Manifest [V]) : Unit = {
+        manifest.typeArguments  must_==  args.toList
+    }
+
     // ===============================================================================================
     // ===============================================================================================
     // ===============================================================================================
@@ -66,13 +83,58 @@ class JdbcStatementsTest extends JacoreSpecWithJUnit ("Statement specification")
     "Statement0" should {
         "be constructable from strings" in {
             checkStmt (s0_2, "set xxx")
+            checkArgs (s0_2, manifest [Domainless])
+            s0_2.domainPlaceholders  must_==  Vector ()
+
             checkStmt (s0_1, "select 1")
+            checkArgs (s0_1, manifest [Domainless])
+            s0_1.domainPlaceholders  must_==  Vector ()
+        }
+
+        "have domain orianted statements" in {
+            val x = X (x = 100500, y = "asd")
+            val sd0_1_dps = sd0_1.domainPlaceholders
+            checkStmt (sd0_1, "select 1 ?")
+            checkArgs (sd0_1, manifest [X])
+            sd0_1_dps.map (_.jdbcType)  must_==  Vector (JdbcInt)
+            sd0_1_dps.map (_.f (x))     must_==  Vector (100500)
+            sd0_1_dps.map (_.position)  must_==  Vector (1)
+
+            val y = Y (a = "a", b = 3)
+            val sd0_2_dps = sd0_2.domainPlaceholders
+            checkStmt (sd0_2, "set xxx ? ?")
+            checkArgs (sd0_2, manifest [Y])
+            sd0_2_dps.map (_.jdbcType)  must_==  Vector (JdbcString, JdbcInt)
+            sd0_2_dps.map (_.f (y))     must_==  Vector ("a", 3)
+            sd0_2_dps.map (_.position)  must_==  Vector (1, 2)
+
+            val sd0_3_dps = sd0_3.domainPlaceholders
+            checkStmt (sd0_3, "select 1 ? abc ?")
+            checkArgs (sd0_3, manifest [X])
+            sd0_3_dps.map (_.jdbcType)  must_==  Vector (JdbcInt, JdbcString)
+            sd0_3_dps.map (_.f (x))     must_==  Vector (100500, "asd")
+            sd0_3_dps.map (_.position)  must_==  Vector (1, 2)
+        }
+
+        "support concatenation with strings for domain orianted statements" in {
+            val s = sd0_1 ++ "blah"
+
+            val x = X (x = 99, y = "omg")
+            val s_dps = s.domainPlaceholders
+            checkStmt (s, "select 1 ? blah")
+            checkArgs (s, manifest [X])
+            s_dps.map (_.jdbcType)  must_==  Vector (JdbcInt)
+            s_dps.map (_.f (x))     must_==  Vector (99)
+            s_dps.map (_.position)  must_==  Vector (1)
         }
 
         "support concatenation with strings" in {
             checkStmt (s0_1 ++ "from A",      "select 1 from A")
             checkStmt (s0_2 ++ "1",           "set xxx 1")
             checkStmt (s0_vstring ++ "abc",   "set xxx ? abc",  Prov (JdbcString, "xxx", 1))
+
+            checkArgs (s0_vstring, manifest [Domainless])
+            s0_vstring.domainPlaceholders  must_==  Vector ()
         }
 
         "support concatenation with provided values" in {
@@ -81,27 +143,41 @@ class JdbcStatementsTest extends JacoreSpecWithJUnit ("Statement specification")
 
             s0_vint     must haveClass [Statement0 [_]]
             s0_vstring  must haveClass [Statement0 [_]]
+
+            checkArgs (s0_vint, manifest [Domainless])
+            s0_vint.domainPlaceholders  must_==  Vector ()
+        }
+
+        "support concatenation with provided values for domain orianted statements" in {
+            val s = sd0_1 ++ (JdbcLong, 30L)
+
+            val x = X (x = 5, y = "ox")
+            val s_dps = s.domainPlaceholders
+            checkStmt (s, "select 1 ? ?", Prov (JdbcLong, 30L, 2))
+            checkArgs (s, manifest [X])
+            s_dps.map (_.jdbcType)  must_==  Vector (JdbcInt)
+            s_dps.map (_.f (x))     must_==  Vector (5)
+            s_dps.map (_.position)  must_==  Vector (1)
         }
 
         "construct Statement1 objects" in {
-            def checkArgs (manifest : Manifest [_], args : Manifest [_]*) : Unit = {
-                manifest.typeArguments.drop (1)  must_==  args.toList
-            }
-
             def check [T <: AbstractJdbcType [_], S <: Statement1 [_, T]]
-                        (s : S, t : T, p : Int = 1) (implicit sm : Manifest [S], tm : Manifest [T]) : Unit =
+                        (dm : Manifest [_], s : S, t : T, p : Int = 1) (
+                        implicit sm : Manifest [S], tm : Manifest [T]) : Unit =
             {
-                checkArgs (sm, tm)
+                checkArgs (sm, dm, tm)
 
                 (s.placeholder : Hdr [T])  must_==  Hdr (t, p)
             }
 
-            check (s1_int,      JdbcInt)
-            check (s1_string,   JdbcString)
-            check (s1_blob,     JdbcBlob)
-            check (s1_clob,     JdbcClob)
-            check (s1_int_vstr, JdbcInt)
-            check (s1_vint_str, JdbcString, 2)
+            val domainless = manifest [Domainless]
+
+            check (domainless, s1_int,      JdbcInt)
+            check (domainless, s1_string,   JdbcString)
+            check (domainless, s1_blob,     JdbcBlob)
+            check (domainless, s1_clob,     JdbcClob)
+            check (domainless, s1_int_vstr, JdbcInt)
+            check (domainless, s1_vint_str, JdbcString, 2)
 
             checkStmt (s1_int,      "select 1 ?")
             checkStmt (s1_string,   "select 1 ?")
@@ -111,6 +187,24 @@ class JdbcStatementsTest extends JacoreSpecWithJUnit ("Statement specification")
             checkStmt (s1_vint_str, "insert into x values ( ? , ? )", Prov (JdbcInt, 10, 1))
 
             s1_int  must haveClass [Statement1 [_, _]]
+
+            s1_int.domainPlaceholders       must_==  Vector ()
+            s1_string.domainPlaceholders    must_==  Vector ()
+            s1_blob.domainPlaceholders      must_==  Vector ()
+            s1_clob.domainPlaceholders      must_==  Vector ()
+            s1_int_vstr.domainPlaceholders  must_==  Vector ()
+            s1_vint_str.domainPlaceholders  must_==  Vector ()
+
+            // sd1
+            val y = Y (a = "aa", b = 33)
+            val sd1_dps = sd1.domainPlaceholders
+            check (manifest [Y], sd1, JdbcRef, p = 3)
+            checkStmt (sd1, "set xxx ? ? ?")
+            checkArgs (sd1, manifest [Y], manifestOf (JdbcRef))
+            sd1 must haveClass [Statement1 [_, _]]
+            sd1_dps.map (_.jdbcType)  must_==  Vector (JdbcString, JdbcInt)
+            sd1_dps.map (_.f (y))     must_==  Vector ("aa", 33)
+            sd1_dps.map (_.position)  must_==  Vector (1, 2)
         }
     }
 
@@ -125,16 +219,20 @@ class JdbcStatementsTest extends JacoreSpecWithJUnit ("Statement specification")
 
             checkStmt (s, "select 1 ? from ABC")
             checkPlaceholder (s.placeholder,  JdbcInt, 1)
+            checkArgs (s, manifest [Domainless], manifestOf (JdbcInt))
+            s.domainPlaceholders  must_==  Vector ()
         }
 
         "support concatenation with provided values" in {
             val ls2 = "a" ++ JdbcBytes ++ (JdbcInt, 100)
             checkStmt (ls2, "a ? ?", Prov (JdbcInt, 100, 2))
             checkPlaceholder (ls2.placeholder, JdbcBytes, 1)
+            ls2.domainPlaceholders  must_==  Vector ()
 
             val ls3 = "a" ++ (JdbcString, "xyz") ++ "x" ++ JdbcBytes ++ (JdbcInt, 50)
             checkStmt (ls3, "a ? x ? ?", Prov (JdbcString, "xyz", 1), Prov (JdbcInt, 50, 3))
             checkPlaceholder (ls3.placeholder, JdbcBytes, 2)
+            ls3.domainPlaceholders  must_==  Vector ()
         }
 
         "construct Statement2 objects" in {
@@ -152,6 +250,8 @@ class JdbcStatementsTest extends JacoreSpecWithJUnit ("Statement specification")
             checkPlaceholder (s2_vint_str_blob.placeholder2, JdbcBlob, 3)
 
             s2_int_blob  must haveClass [Statement2 [_, _, _]]
+            checkArgs (s2_int_blob, manifest [Domainless], manifestOf (JdbcInt), manifestOf (JdbcBlob))
+            s2_int_blob.domainPlaceholders  must_==  Vector ()
         }
     }
 
@@ -169,6 +269,7 @@ class JdbcStatementsTest extends JacoreSpecWithJUnit ("Statement specification")
             checkPlaceholder (s3_int_blob_short_vstr.placeholder1, JdbcInt, 1)
             checkPlaceholder (s3_int_blob_short_vstr.placeholder2, JdbcBlob, 2)
             checkPlaceholder (s3_int_blob_short_vstr.placeholder3, JdbcShort, 3)
+            s3_int_blob_short_vstr.domainPlaceholders  must_==  Vector ()
 
             // - - -  - - -
 
@@ -181,6 +282,12 @@ class JdbcStatementsTest extends JacoreSpecWithJUnit ("Statement specification")
             checkPlaceholder (s3_int_vstr_str_ref.placeholder3, JdbcRef, 4)
 
             s3_int_vstr_str_ref  must haveClass [Statement3 [_, _, _, _]]
+            checkArgs (s3_int_vstr_str_ref,
+                       manifest [Domainless],
+                       manifestOf (JdbcInt),
+                       manifestOf (JdbcString),
+                       manifestOf (JdbcRef))
+            s3_int_vstr_str_ref.domainPlaceholders  must_==  Vector ()
         }
     }
 
@@ -201,6 +308,13 @@ class JdbcStatementsTest extends JacoreSpecWithJUnit ("Statement specification")
             checkPlaceholder (s4.placeholder4, JdbcBytes, 5)
 
             s4  must haveClass [Statement4 [_, _, _, _, _]]
+            checkArgs (s4,
+                       manifest [Domainless],
+                       manifestOf (JdbcInt),
+                       manifestOf (JdbcBlob),
+                       manifestOf (JdbcShort),
+                       manifestOf (JdbcBytes))
+            s4.domainPlaceholders  must_==  Vector ()
         }
     }
 
@@ -222,6 +336,14 @@ class JdbcStatementsTest extends JacoreSpecWithJUnit ("Statement specification")
             checkPlaceholder (s5.placeholder5, JdbcLong, 6)
 
             s5  must haveClass [Statement5 [_, _, _, _, _, _]]
+            checkArgs (s5,
+                       manifest [Domainless],
+                       manifestOf (JdbcInt),
+                       manifestOf (JdbcBlob),
+                       manifestOf (JdbcShort),
+                       manifestOf (JdbcBytes),
+                       manifestOf (JdbcLong))
+            s5.domainPlaceholders  must_==  Vector ()
         }
     }
 
@@ -247,6 +369,15 @@ class JdbcStatementsTest extends JacoreSpecWithJUnit ("Statement specification")
             checkPlaceholder (s.placeholder6, JdbcInt, 8)
 
             s  must haveClass [Statement6 [_, _, _, _, _, _, _]]
+            checkArgs (s,
+                       manifest [Domainless],
+                       manifestOf (JdbcInt),
+                       manifestOf (JdbcBlob),
+                       manifestOf (JdbcShort),
+                       manifestOf (JdbcBytes),
+                       manifestOf (JdbcLong),
+                       manifestOf (JdbcInt))
+            s.domainPlaceholders  must_==  Vector ()
         }
     }
 
@@ -273,6 +404,16 @@ class JdbcStatementsTest extends JacoreSpecWithJUnit ("Statement specification")
             checkPlaceholder (s.placeholder7, JdbcString, 9)
 
             s  must haveClass [Statement7 [_, _, _, _, _, _, _, _]]
+            checkArgs (s,
+                       manifest [Domainless],
+                       manifestOf (JdbcInt),
+                       manifestOf (JdbcBlob),
+                       manifestOf (JdbcShort),
+                       manifestOf (JdbcBytes),
+                       manifestOf (JdbcLong),
+                       manifestOf (JdbcInt),
+                       manifestOf (JdbcString))
+            s.domainPlaceholders  must_==  Vector ()
         }
     }
 
@@ -300,6 +441,7 @@ class JdbcStatementsTest extends JacoreSpecWithJUnit ("Statement specification")
             checkPlaceholder (s.placeholder8, JdbcInt, 10)
 
             s  must haveClass [Statement8 [_, _, _, _, _, _, _, _, _]]
+            s.domainPlaceholders  must_==  Vector ()
         }
     }
 
@@ -328,6 +470,18 @@ class JdbcStatementsTest extends JacoreSpecWithJUnit ("Statement specification")
             checkPlaceholder (s.placeholder9, JdbcBytes, 11)
 
             s  must haveClass [Statement9 [_, _, _, _, _, _, _, _, _, _]]
+            checkArgs (s,
+                       manifest [Domainless],
+                       manifestOf (JdbcInt),
+                       manifestOf (JdbcBlob),
+                       manifestOf (JdbcShort),
+                       manifestOf (JdbcBytes),
+                       manifestOf (JdbcLong),
+                       manifestOf (JdbcInt),
+                       manifestOf (JdbcString),
+                       manifestOf (JdbcInt),
+                       manifestOf (JdbcBytes))
+            s.domainPlaceholders  must_==  Vector ()
         }
     }
 
@@ -359,6 +513,68 @@ class JdbcStatementsTest extends JacoreSpecWithJUnit ("Statement specification")
             checkPlaceholder (s.placeholder10, JdbcInt, 13)
 
             s  must haveClass [Statement10 [_, _, _, _, _, _, _, _, _, _, _]]
+            checkArgs (s,
+                       manifest [Domainless],
+                       manifestOf (JdbcInt),
+                       manifestOf (JdbcBlob),
+                       manifestOf (JdbcShort),
+                       manifestOf (JdbcBytes),
+                       manifestOf (JdbcLong),
+                       manifestOf (JdbcInt),
+                       manifestOf (JdbcString),
+                       manifestOf (JdbcInt),
+                       manifestOf (JdbcBytes),
+                       manifestOf (JdbcInt))
+            s.domainPlaceholders  must_==  Vector ()
+        }
+    }
+
+    // ===============================================================================================
+    // ===============================================================================================
+    // ===============================================================================================
+    // ===============================================================================================
+
+    "Statement10" should {
+        "work with domain objects" in {
+            val s = sd10
+            val dps = s.domainPlaceholders
+
+            checkStmt (s,
+                       "select 1 ? ? test ? ? blah ? ? ? ? ? ? ? ? ? x ? ?",
+                       Prov (JdbcString, "x", 4),
+                       Prov (JdbcString, "test", 7),
+                       Prov (JdbcLong, 10L, 12),
+                       Prov (JdbcFloat, 1.0f, 14))
+
+            checkPlaceholder (s.placeholder1, JdbcInt, 1)
+            checkPlaceholder (s.placeholder2, JdbcBlob, 2)
+            checkPlaceholder (s.placeholder3, JdbcShort, 3)
+            checkPlaceholder (s.placeholder4, JdbcBytes, 5)
+            checkPlaceholder (s.placeholder5, JdbcLong, 6)
+            checkPlaceholder (s.placeholder6, JdbcInt, 8)
+            checkPlaceholder (s.placeholder7, JdbcString, 9)
+            checkPlaceholder (s.placeholder8, JdbcInt, 10)
+            checkPlaceholder (s.placeholder9, JdbcBytes, 11)
+            checkPlaceholder (s.placeholder10, JdbcInt, 13)
+
+            s  must haveClass [Statement10 [_, _, _, _, _, _, _, _, _, _, _]]
+            checkArgs (s,
+                       manifest [X],
+                       manifestOf (JdbcInt),
+                       manifestOf (JdbcBlob),
+                       manifestOf (JdbcShort),
+                       manifestOf (JdbcBytes),
+                       manifestOf (JdbcLong),
+                       manifestOf (JdbcInt),
+                       manifestOf (JdbcString),
+                       manifestOf (JdbcInt),
+                       manifestOf (JdbcBytes),
+                       manifestOf (JdbcInt))
+
+            val x = X (x = 91, y = "yyyy")
+            dps.map (_.jdbcType)  must_==  Vector (JdbcString)
+            dps.map (_.f (x))     must_==  Vector ("yyyy")
+            dps.map (_.position)  must_==  Vector (15)
         }
     }
 }
